0d5a09d59b3a421526657ec6730fa23bf095da7c
Krishnamurti Pandaram <krishna916@outlook.com>
2026-07-02 23:56:55 +0530

    config: tighten dockerfile packages and align event_publication table schema with modulith

--- server/src/main/resources/db/migration/V1__create_event_publication.sql
+++ server/src/main/resources/db/migration/V1__create_event_publication.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS event_publication (
     id uuid NOT NULL,
-    listener_id character varying(255) NOT NULL,
-    event_type character varying(255) NOT NULL,
-    serialized_event character varying(255) NOT NULL,
+    listener_id text NOT NULL,
+    event_type text NOT NULL,
+    serialized_event text NOT NULL,
     publication_date timestamp with time zone NOT NULL,
     completion_date timestamp with time zone,
     completion_attempts integer NOT NULL,
     last_resubmission_date timestamp(6) with time zone,
-    status character varying(255),
+    status text,
     CONSTRAINT event_publication_pkey PRIMARY KEY (id),
-    CONSTRAINT event_publication_status_check CHECK (((status)::text = ANY ((ARRAY['PUBLISHED'::character varying, 'PROCESSING'::character varying, 'COMPLETED'::character varying, 'FAILED'::character varying, 'RESUBMITTED'::character varying])::text[])))
+    CONSTRAINT event_publication_status_check CHECK (status IN ('PUBLISHED', 'PROCESSING', 'COMPLETED', 'FAILED', 'RESUBMITTED'))
 );
 
 CREATE INDEX IF NOT EXISTS event_publication_by_completion_date_idx ON event_publication USING btree (completion_date);
-CREATE INDEX IF NOT EXISTS event_publication_serialized_event_hash_idx ON event_publication USING hash (serialized_event);
+CREATE INDEX IF NOT EXISTS event_publication_serialized_event_hash_idx ON event_publication (listener_id, serialized_event);
